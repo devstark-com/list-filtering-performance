ee31247161ffe0cb0f79865c9a44576906a80406
Refat <nobilix@gmail.com>
2017-02-23 17:19:15 +0300

    dummy data and components

---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <item v-for="item in list" :itemData="item"></item>
+  </div>
+</template>
+
+<script>
+import Faker from 'faker'
+import Vue from 'vue'
+import Item from './item/Item.vue'
+
+export default {
+  components: {
+    Item
+  },
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    for (let i = 0; i < 5000; i++) {
+      var item = {
+        id: i,
+        name: Faker.name.findName()
+      }
+      Vue.set(this.list, i, item)
+    }
+  }
+}
+</script>
+
---
+++ src/components/item/Item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="item">
+      {{ itemData.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['itemData'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
